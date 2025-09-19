0fce1465ed15c8386e70e86120900c6c201366c0
Ahmed <Ammed@Ali>
2025-09-19 15:56:32 +0300

    feat(auth): add authentication service with register/login logic. added migration file for the new Database

---
+++ prisma/migrations/20250919123204_add_refresh_token_model_password/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `password` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `password` VARCHAR(191) NOT NULL;
+
+-- CreateTable
+CREATE TABLE `refresh_tokens` (
+    `id` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `expiresAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `refresh_tokens_token_key`(`token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `refresh_tokens` ADD CONSTRAINT `refresh_tokens_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
